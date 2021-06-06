6e8dc17bbd241f21c1bb3981efd8b32d47b46961
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-06-06 12:33:32 +0200

    :recycle: converted avatar to webp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -49,7 +49,7 @@
 		</div>
 	</div>
 	<div class="avatar-blobs">
-		<img src="avatar.png" alt="Me" />
+		<img src="/avatar.webp" alt="Me" />
 
 		<svg viewBox="0 0 503 375" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
