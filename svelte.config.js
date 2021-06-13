ab5d13ba9d3cb2c6981bcd7a373f8f5431bb9a88
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-06-13 09:15:39 +0200

    :sparkles: use svelte-adders to add postcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
-import autoprefixer from 'autoprefixer';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({postcss: {
-		plugins: [autoprefixer()]
-		}}),
+	preprocess: [preprocess({
+		"postcss": true
+	})],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
