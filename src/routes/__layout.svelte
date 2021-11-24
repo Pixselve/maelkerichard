f936bfefc350b77c062b152a64fc55d90af1d1f8
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-11-24 15:01:37 +0100

    :sparkles: page title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Mael Kerichard</title>
+</svelte:head>
 <main class='xl:max-w-7xl p-7 mt-0 xl:mt-40 m-auto space-y-20 text-gray-800'>
 	<slot />
 </main>
