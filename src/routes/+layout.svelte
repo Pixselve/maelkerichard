1d9bd4652f2cda864d5cdcd0a98899d8246b8fef
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-10-01 20:46:19 +0200

    chore: remove blog + update dependencies

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+import '../app.postcss';
+import Logo from '$lib/Logo.svg';
+</script>
+
+<nav class='flex justify-between p-5 max-w-6xl m-auto'>
+	<div>
+		<img class='h-6' src={Logo} alt='Logo'>
+	</div>
+</nav>
+
+<main class='max-w-6xl p-5 m-auto'>
+	<slot />
+</main>
