792cfa4d9ff1c6f6b19954edb222537cc23797e5
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-06 18:55:49 +0100

    fix: fix website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://mael.app',
   integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })]
