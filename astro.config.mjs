65af2360a1497d692292f468a6f4b5c3090f6e27
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-06 18:39:25 +0100

    feat: robot txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,7 +10,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image({
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })]
 });
