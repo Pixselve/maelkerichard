d17c50e57f96694b6e970751ed70b06b168b275e
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-12 15:52:03 +0100

    feat: added github and linkedin links

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,10 +1,10 @@
 import { getContributionGraph } from '$lib/contributionGraphData';
 
 /** @type {import('./$types').PageLoad} */
-export const load = async ({ setHeaders }) => {
+export const load = async ({ setHeaders }: { setHeaders: any }) => {
 	const contributionGraph = await getContributionGraph();
 	setHeaders({
-		'Cache-Control': "max-age=86400"
+		'Cache-Control': 'max-age=86400'
 	});
 	return {
 		contributionGraph
