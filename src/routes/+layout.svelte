3a296fe64a22b3166cd846b207efd228f40b5714
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-18 19:40:49 +0100

    feat: remove svelte SEO and use svelte-meta-tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,62 +1,70 @@
-<script lang="ts">
-  import SvelteSeo from "svelte-seo";
-  import "../app.postcss";
+<script lang='ts'>
+import '../app.postcss';
+import { JsonLd, MetaTags } from 'svelte-meta-tags';
 </script>
 
 <svelte:head>
-  <meta name="description" content="A french student enrolled in an engineering degree in computer science.">
+	<MetaTags canonical='https://www.mael.app'
+						description='A french student enrolled in an engineering degree in computer science.'
+						openGraph={{
+    type: 'website',
+    url: 'https://mael.app',
+    title: 'Mael Kerichard',
+    description: 'A french student enrolled in an engineering degree in computer science.',
+    images: [
+      {
+        url: 'https://mael.app/social.png',
+        width: 2388,
+        height: 720,
+        alt: 'Mael Kerichard',
+      },
+    ]
+  }}
+						twitter='{{
+							site: "@pixselve",
+							card: "summary_large_image",
+							title: "Mael Kerichard",
+							description: "A french student enrolled in an engineering degree in computer science.",
+							image: "https://mael.app/social.png",
+							imageAlt: "Mael Kerichard",
+							handle: "@pixselve"
+						}}'></MetaTags>
+	<JsonLd schema={{
+	                   '@type': 'Person',
+	                    name: 'Mael Kerichard',
+	                    url: 'https://mael.app',
+	                    sameAs: [
+	                      'https://www.linkedin.com/in/maelkerichard/',
+	                      'https://github.com/Pixselve'
+	                      ],
+	                    image: 'https://mael.app/social.png',
+	                    jobTitle: 'Computer Science Student',
+	                    worksFor: {
+	                      '@type': 'Organization',
+	                      name: 'École supérieure d\'ingénieurs de Rennes (ESIR)'
+	                    },
+	                    address: {
+	                      '@type': 'PostalAddress',
+	                      addressLocality: 'Rennes',
+	                      addressRegion: 'Brittany',
+	                      postalCode: '35000',
+	                      addressCountry: 'France'
+	                    },
 
-  <meta property="og:url" content="https://mael.app">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="Mael Kerichard">
-  <meta property="og:description" content="A french student enrolled in an engineering degree in computer science.">
-  <meta property="og:image" content="https://mael.app/social.png">
 
-  <meta name="twitter:card" content="summary_large_image">
-  <meta property="twitter:domain" content="mael.app">
-  <meta property="twitter:url" content="https://mael.app">
-  <meta name="twitter:title" content="Mael Kerichard">
-  <meta name="twitter:description" content="A french student enrolled in an engineering degree in computer science.">
-  <meta name="twitter:image" content="https://mael.app/social.png">
-
-  <SvelteSeo
-
-             jsonLd={{
-                   '@type': 'Person',
-                    name: 'Mael Kerichard',
-                    url: 'https://mael.app',
-                    sameAs: [
-                      'https://www.linkedin.com/in/maelkerichard/',
-                      'https://github.com/Pixselve'
-                      ],
-                    image: 'https://mael.app/social.png',
-                    jobTitle: 'Computer Science Student',
-                    worksFor: {
-                      '@type': 'Organization',
-                      name: 'École supérieure d\'ingénieurs de Rennes (ESIR)'
-                    },
-                    address: {
-                      '@type': 'PostalAddress',
-                      addressLocality: 'Rennes',
-                      addressRegion: 'Brittany',
-                      postalCode: '35000',
-                      addressCountry: 'France'
-                    },
-
-             }}
-  ></SvelteSeo>
+	}}></JsonLd>
 </svelte:head>
 
-<main class="min-h-screen bg-slate-900 flex flex-col">
-  <nav class="text-slate-50 p-4">
-    <div class="flex justify-end space-x-10 max-w-6xl m-auto">
-      <a class="text-xl font-bold uppercase" href="/">🏠 Home</a>
-      <a class="text-xl font-bold uppercase" href="/projects">🗂️ Projects</a>
-    </div>
-  </nav>
+<main class='min-h-screen bg-slate-900 flex flex-col'>
+	<nav class='text-slate-50 p-4'>
+		<div class='flex justify-end space-x-10 max-w-6xl m-auto'>
+			<a class='text-xl font-bold uppercase' href='/'>🏠 Home</a>
+			<a class='text-xl font-bold uppercase' href='/projects'>🗂️ Projects</a>
+		</div>
+	</nav>
 
 
-  <slot></slot>
+	<slot></slot>
 
 
 </main>
