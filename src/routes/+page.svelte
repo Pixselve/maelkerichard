d4ae7bb84d1da835fae7e27957d24c7aa678d34c
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-18 19:59:43 +0100

    feat: aria-label for links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,14 +20,16 @@ import logo from '$lib/assets/mael.png?w=300;400;500&format=webp&srcset';
 				I'm ready to help make your project a success.
 			</p>
 			<div class='space-x-4 flex'>
-				<a class='fill-white/50 hover:fill-white' href='https://www.linkedin.com/in/maelkerichard/' rel='noopener noreferrer'
+				<a aria-label='LinkedIn' class='fill-white/50 hover:fill-white'
+					 href='https://www.linkedin.com/in/maelkerichard/' rel='noopener noreferrer'
 					 target='_blank'>
 					<svg class='h-10 w-10' viewBox='0 0 24 24'>
 						<path
 							d='M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z' />
 					</svg>
 				</a>
-				<a class='fill-white/50 hover:fill-white' href='https://github.com/Pixselve' rel='noopener noreferrer'
+				<a aria-label='GitHub' class='fill-white/50 hover:fill-white' href='https://github.com/Pixselve'
+					 rel='noopener noreferrer'
 					 target='_blank'>
 					<svg class='h-10 w-10' viewBox='0 0 24 24'>
 						<path
