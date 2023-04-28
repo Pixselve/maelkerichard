eccd0e4562d90abec24461cb3b4bfcc9b7ab177a
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-04-28 22:15:45 +0200

    feat: added MacStats

--- src/routes/projects/projects.ts
+++ src/routes/projects/projects.ts
@@ -11,16 +11,25 @@ export const projects: { name: string; projects: ProjectType[] }[] = [
 		name: 'Web Development',
 		projects: [
 			{
-				name: "QuizCraft",
-				description: "A web app to convert JSON into a Google Forms quiz.",
+				name: 'QuizCraft',
+				description: 'A web app to convert JSON into a Google Forms quiz.',
 				links: [
 					{ name: 'Code', url: 'https://github.com/Pixselve/quizcraft' },
-					{ name: 'Website', url: 'https://quizcraft.mael.app' }
+					{ name: 'Hosted version', url: 'https://quizcraft.mael.app' }
 				],
 				tags: ['React', 'TypeScript', 'Google Forms', 'JSON']
 			},
 			{
-				name: "Kitten Asso's",
+				name: 'MacStats',
+				description: 'A web app to display stats about McDonald\'s restaurants in France.',
+				links: [
+					{ name: 'Code', url: 'https://github.com/Pixselve/macstats' },
+					{ name: 'Hosted version', url: 'https://macstats.mael.app' }
+				],
+				tags: ['Svelte', 'Data Visualization', '2-person team', 'REST API']
+			},
+			{
+				name: 'Kitten Asso\'s',
 				description:
 					'Two projects made at school to learn web development: a REST API made with NestJS and a frontend with Angular.',
 				links: [
@@ -44,7 +53,7 @@ export const projects: { name: string; projects: ProjectType[] }[] = [
 		name: 'Software Architecture',
 		projects: [
 			{
-				name: "Kitten Asso's",
+				name: 'Kitten Asso\'s',
 				description:
 					'Complete software architecture of a web application using real world tools and technologies like Docker, Github Actions, Quarkus, PostgreSQL, etc.',
 				links: [
@@ -63,21 +72,21 @@ export const projects: { name: string; projects: ProjectType[] }[] = [
 			{
 				name: 'Ariadne',
 				description:
-					"Implementation of a privacy preserving network protocol's client and server on top of a IPv6 based on a paper.",
+					'Implementation of a privacy preserving network protocol\'s client and server on top of a IPv6 based on a paper.',
 				links: [],
 				tags: ['Networking', 'POC', 'Rust', 'Cryptographic', 'Internship']
 			},
 			{
 				name: 'Gitlab CI documentation generator',
 				description:
-					"A CLI tool to generate documentation for Gitlab CI pipelines made in one night during the Nuit de l'info 2022.",
+					'A CLI tool to generate documentation for Gitlab CI pipelines made in one night during the Nuit de l\'info 2022.',
 				links: [
 					{
 						name: 'Code',
 						url: 'https://github.com/Pixselve/gitlab-pipeline-to-md'
 					},
 					{
-						name: "Nuit de l'info",
+						name: 'Nuit de l\'info',
 						url: 'https://www.nuitdelinfo.com'
 					}
 				],
@@ -89,12 +98,12 @@ export const projects: { name: string; projects: ProjectType[] }[] = [
 		name: 'Game Development',
 		projects: [
 			{
-				name: "Thumbthing's Wrong",
+				name: 'Thumbthing\'s Wrong',
 				description:
-					"A game made in 48 hours for the ESIR Game Jam 2023. It's a multiplayer game made with Unreal Engine. A player is on PC and has to jump from platform to platform and up to 4 players on a mobile web app who have to help (or not) the main player to advance using cards that break the rules of the game.",
+					'A game made in 48 hours for the ESIR Game Jam 2023. It\'s a multiplayer game made with Unreal Engine. A player is on PC and has to jump from platform to platform and up to 4 players on a mobile web app who have to help (or not) the main player to advance using cards that break the rules of the game.',
 				links: [
 					{
-						name: 'Web app',
+						name: 'Web app code',
 						url: 'https://github.com/Pixselve/thumbthings-wrong-client'
 					}
 				],
