0431c50a92f77414bc2f1a3fea92955365d97876
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-08-19 18:08:09 +0100

    Updates the description of my internship at Amazon

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -69,7 +69,22 @@ const events = [
 		timeDate: new Date('2023-06'),
 		title: 'Internship as a Software Development Engineer at Amazon Dublin',
 		description:
-			'TBA'
+			`As a Software Development Engineer Intern at Amazon, I contributed to the development and deployment of a data-driven workflow.
+
+Responsibilities:
+- Developed and implemented backend functionalities utilizing AWS services such as Lambda, DynamoDB, S3, and Redshift.
+- Wrote and executed unit tests and integration tests to ensure the software's reliability and performance.
+- Assisted in the architecture and deployment of cloud infrastructure using AWS Cloud Development Kit (CDK) and AWS CloudFormation.
+- Participated in the setup and maintenance of CI/CD pipelines to automate testing and deployment processes.
+
+Achievements:
+- Successfully applied Amazon's Leadership Principles, emphasizing customer obsession, ownership, and long-term thinking.
+
+Skills Gained:
+- Gained in-depth knowledge of AWS services and cloud architectures.
+- Improved software testing and automation skills through hands-on experience with CI/CD pipelines.
+- Developed effective collaboration and communication skills by working in a fast-paced, agile environment.
+- Honed problem-solving skills and learned best practices for software development from industry experts.`
 
 	}
 ].sort((a, b) => b.timeDate.getTime() - a.timeDate.getTime());
