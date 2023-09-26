7337a8d7792aaab8913a7d6dfd37a04cb50e8fb0
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-09-26 21:07:37 +0200

    feat: robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import mdx from "@astrojs/mdx";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mael.app",
-  integrations: [sitemap(), tailwind(), prefetch(), mdx()]
+  integrations: [sitemap(), tailwind(), prefetch(), mdx(), robotsTxt()]
 });
